19a5a866dad7caaeb8fbdeaa2facb4d121c3121e
Lscutelx <lscutelx@gmail.com>
2024-04-01 22:58:04 +0300

    Create separating word function

--- javascript.js
+++ javascript.js
@@ -17,4 +17,52 @@ function showSmoothly(elem) {
     elem.animate(keyframes, options)
 }
 
-showSmoothly(word)
+/* showSmoothly(word) */
+
+
+let container = document.querySelector('.wrap');
+let arrayLetters = word.innerText.split('');
+console.log(arrayLetters)
+
+container.innerHTML = '';
+
+function insertLetter() {
+    arrayLetters.forEach(letter => {
+        let wrapForLetter = document.createElement('span');
+        wrapForLetter.classList = 'letter';
+        wrapForLetter.textContent = letter;
+        container.appendChild(wrapForLetter)
+    })
+}
+insertLetter()
+
+let arrayLettersHtml = document.querySelectorAll('.letter');
+console.log(arrayLettersHtml)
+
+let timing = 0;
+/* arrayLettersHtml.forEach((letterTag) => {
+    console.log(letterTag);
+    const keyframes = new KeyframeEffect(
+        letterTag,
+        [
+            { 
+                opacity: 0,
+                transform: "translateY(-50px)"
+            },
+            { 
+                opacity: 1,
+                transform: "translateY(0px)"
+            }
+        ],
+        {
+            duration: 2000,
+            delay: timing
+        }
+    );
+    const animation = new Animation(
+        keyframes,
+        document.timeline,
+    );
+    animation.play();
+    timing += 1000;
+}) */
